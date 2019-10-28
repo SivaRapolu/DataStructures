7eab3190b87d9fe4c9080c034fcf8ca835db0340
SivaRapolu <siva.rapolu@gmail.com>
2019-10-29 00:50:47 +0530

    Add files via upload
    Data Structures and Algorithms - Selection problem K/2.
    Started of with 65 milliseconds.

---
+++ Selection_kby2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DS_Alogorithms
+{
+    class Selection_kby2
+    {
+        public static bool Find(int[] arr, int k)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == k)
+                    return true;
+            }
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+
+            int N = 9999999;
+            int k = N / 2;
+
+            int[] arrNums = new int[N];
+            for(int i = 0; i < N; i++)
+            {
+                arrNums[i] = i;
+            }
+
+            Find(arrNums, k);
+
+            watch.Stop();
+
+            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+        }
+
+        
+    }
+}
